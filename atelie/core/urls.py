91c230d347be36cec3a8edfb2d8908260a81c1e5
emerson-91 <emerson.hcdossantos@gmail.com>
2020-05-12 17:37:48 -0400

    projeto atelie - django - organizacao de paginas

--- atelie/core/urls.py
+++ atelie/core/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from .views import Home, Venda, Consulta, Produto
 urlpatterns = [
-    path('', Home, name='Home' ),
+    path('', Home, name='Home'),
     path('venda/', Venda, name='venda'),
     path('consulta/', Consulta, name='consulta'),
     path('produto', Produto, name='produto')
