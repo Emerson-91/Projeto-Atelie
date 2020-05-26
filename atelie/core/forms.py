70d19d31ba5437bc2fea4d790b381198c36ed43e
emerson-91 <emerson.hcdossantos@gmail.com>
2020-05-26 15:44:45 -0400

    atualizando sistema para pedidos

--- atelie/core/forms.py
+++ atelie/core/forms.py
@@ -8,12 +8,20 @@ class CadastroForm(forms.ModelForm):
 
     class Meta:
         model = Cadastro
-        fields = ['nome', 'telefone','entrega','endereco','qtd', ]
+        fields = ['nome', 'telefone','entrega','endereco', ]
         
 
 
 class ProdutosForm(forms.ModelForm):
     class Meta:
         model = CadProduto
-        fields = ['produto', 'valor']
+        fields = ['produto', 'valor', ]
+
+
+class PedidoForm(forms.ModelForm):
+    class Meta:
+        model = Pedido
+        fields = ['produto', 'desconto', 'qtd' ]
+
+
 
--- atelie/templates/js/estilo.js
+++ atelie/templates/js/estilo.js
@@ -1,4 +1,17 @@
 function Alerta(){
 
-alert("Cadastro Realizado com sucesso!");
+alert("Se não faltou algum campo obrigatorio Salvou");
 }
+
+function Fechar(){
+style.display = 'none';
+}
+$(document).ready(function()){
+    var ShowForm = function(){
+    vat btn = $(this);
+    $.ajax({
+    url: btn.attr("data-url")
+    })
+
+
+}}
