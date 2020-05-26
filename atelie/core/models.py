23ee8c8798e60fd011fa05b9053b40c9eba3b2b7
emerson-91 <emerson.hcdossantos@gmail.com>
2020-05-26 15:39:39 -0400

    adicionando model pedidos

--- atelie/core/models.py
+++ atelie/core/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.conf import settings
+from django.db.models import Sum
+
 
 # Create your models here.
 from django.db.models import CharField
@@ -14,17 +17,14 @@ class CadProduto(models.Model):
     def __str__(self):
         return self.produto
 
+
 class Cadastro(models.Model):
     ENTREGA = (( 'Sim', 'sim'),
     ('Nao', 'nao'),)
-    pedido = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=100)
     telefone = models.CharField(max_length=20,)
     endereco = models.CharField(default=" - ", max_length=100, blank=True)
-    data_pedido = models.DateField(auto_now_add=True)
     entrega = models.CharField(max_length=10, choices=ENTREGA)
-    qtd = models.IntegerField(default=0)
-    nomeProduto = models.ForeignKey(CadProduto, null=True, related_name='prodcadastrado', on_delete=models.CASCADE)
 
 
     class Meta:
@@ -32,3 +32,32 @@ class Cadastro(models.Model):
 
     def __str__(self):
         return self.nome
+
+
+
+class Pedido(models.Model):
+    pedido = models.AutoField(primary_key=True)
+    produto = models.ForeignKey(CadProduto, on_delete=models.CASCADE)
+    cliente = models.ForeignKey(Cadastro, on_delete=models.CASCADE)
+    desconto = models.FloatField(default=0)
+    qtd = models.IntegerField(default=0)
+    data_pedido = models.DateField(auto_now_add=True)
+
+
+
+
+    class Meta:
+        db_table = 'pedido'
+
+    def __str__(self):
+        return self.pedido
+
+    def get_total_item(self):
+        return self.qtd * self.produto.valor
+
+    def get_total_preco(self):
+        return self.get_total_item() - self.desconto
+
+
+
+
