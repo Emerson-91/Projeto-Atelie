ede87e1f401511965ada45ce08b37c0e2e353d6e
emerson-91 <emerson.hcdossantos@gmail.com>
2020-06-05 17:50:19 -0400

    arrumar mascara telefone

--- atelie/core/forms.py
+++ atelie/core/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import TextInput
 from django_filters import DateFilter
 from .models import *
 
@@ -9,7 +10,11 @@ class CadastroForm(forms.ModelForm):
     class Meta:
         model = Cadastro
         fields = ['nome', 'telefone','endereco', ]
-        
+
+    def __init__(self, *args, **kwargs):
+        super(CadastroForm, self).__init__(*args, **kwargs)
+        self.fields['telefone'].widget = TextInput(attrs={'class':'phone-mask'})
+
 
 
 class ProdutosForm(forms.ModelForm):
