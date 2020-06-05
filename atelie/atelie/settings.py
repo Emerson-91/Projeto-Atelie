df9c8b7434be51ef1392a959c951669665dcfe7e
emerson-91 <emerson.hcdossantos@gmail.com>
2020-06-05 14:21:13 -0400

    update

--- atelie/atelie/settings.py
+++ atelie/atelie/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'atelie.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r'/home/python/DEV/Projeto-Atelie/atelie/templates'],
+        'DIRS': [r'/home/manutencao/PycharmProjects/Projeto-Atelie/atelie/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,7 +71,7 @@ TEMPLATES = [
         },
     },
 ]
-# r'/home/manutencao/PycharmProjects/Projeto-Atelie/atelie/templates'
+# r'/home/python/DEV/Projeto-Atelie/atelie/templates'
 WSGI_APPLICATION = 'atelie.wsgi.application'
 
 
